0433dd91c4c86b0643131fd1246a4b573db1ae9d
Stefanie Molin <24376333+stefmolin@users.noreply.github.com>
2020-12-13 11:14:36 -0500

    Updating for newer matplotlib version

--- README.md
+++ README.md
@@ -1,37 +1,37 @@
-# Easily create your own colormaps!
+# Easily create your own colormaps
 Utility functions for working with colors in Python.
 
 ## Setup
 If you don't have matplotlib and numpy installed with Python 3:
-```
+```shell
 pip install -r requirements.txt
 ```
 
 ## Usage
 
 1. Import what we need:
-```
+```python
 import matplotlib.pyplot as plt
 import color_utils
 ```
 
 2. Define your colors with their hex codes (you can omit '#' and use 3 digit shorthand) in the order you wish them to appear:
-```
+```python
 my_colors = ['#B2E4DF', '#C2C1E1']
 ```
 
-3. Convert them to RGB values matplotlib can use (on [0,1]):
-```
+3. Convert them to RGB values matplotlib can use (on [0, 1]):
+```python
 rgbs = color_utils.hex_to_rgb_color_list(my_colors)
 ```
 
 4. Create your colormap:
-```
+```python
 my_cmap = color_utils.blended_cmap(rgbs)
 ```
 
 5. Visualize your colormap:
-```
+```python
 cbar = color_utils.draw_cmap(my_cmap)
 plt.show()
 ```
@@ -40,9 +40,9 @@ plt.show()
 <img src="images/two_color_cmap.png?raw=true" align="center" width="450" alt="Two color colormap">
 
 ## Notes
-You aren't limited to using just 2 colors; in fact, you can use as many as you wish!
+You aren't limited to using just 2 colors; in fact, you can use as many as you wish:
 
-```
+```python
 my_colors = ['#00F', '#B2E4DF', '#C2C1E1', 'C0C0C0', 'EEE', '000000']
 rgbs = color_utils.hex_to_rgb_color_list(my_colors)
 
--- color_utils.py
+++ color_utils.py
@@ -26,12 +26,12 @@ def hex_to_rgb_color_list(colors):
 
         if hex_length not in [3, 6]:
             raise ValueError(
-                'Your colors must be of the form #FFF or #FFFFFF'
+                'Your colors must be of the form #FFFFFF or #FFF'
             )
 
         regex = '.' * (hex_length // 3)
         colors[i] = [
-            int(val * (6 // hex_length), 16)/255 \
+            int(val * (6 // hex_length), 16) / 255
             for val in re.findall(regex, color)
         ]
 
@@ -39,22 +39,22 @@ def hex_to_rgb_color_list(colors):
 
 def blended_cmap(rgb_color_list):
     """
-    Create a colormap blending from one color to the other.
+    Created a colormap blending from one color to the other.
 
     Parameters:
-        - rgb_color_list: A list of colors represented as [R, G, B] values
-                          in the range [0, 1], like [[0, 0, 0], [1, 1, 1]],
-                          for black and white, respectively.
+        - rgb_color_list: A list of colors represented as [R, G, B]
+          values in the range [0, 1], like [[0, 0, 0], [1, 1, 1]],
+          for black and white, respectively.
 
     Returns:
         A matplotlib ListedColormap object with your colormap.
     """
     if not isinstance(rgb_color_list, list):
-        raise ValueError('Colors must be passed as a list!')
+        raise ValueError('Colors must be passed as a list.')
     elif len(rgb_color_list) < 2:
         raise ValueError('Must specify at least 2 colors.')
     elif (
-        not isinstance(rgb_color_list[0], list) \
+        not isinstance(rgb_color_list[0], list)
         or not isinstance(rgb_color_list[1], list)
     ) or (
         len(rgb_color_list[0]) != 3 or len(rgb_color_list[1]) != 3
@@ -63,8 +63,7 @@ def blended_cmap(rgb_color_list):
             'Each color should be represented as a list of size 3.'
         )
 
-    N = 256
-    entries = 4 # red, green, blue, alpha
+    N, entries = 256, 4 # red, green, blue, alpha
     rgbas = np.ones((N, entries))
 
     segment_count = len(rgb_color_list) - 1
@@ -78,7 +77,7 @@ def blended_cmap(rgb_color_list):
             offset = 0 if not remainder or seg > 1 else remainder
 
             updates.append(np.linspace(
-                start=rgb_color_list[seg-1][i],
+                start=rgb_color_list[seg - 1][i],
                 stop=rgb_color_list[seg][i],
                 num=segment_size + offset
             ))
@@ -94,13 +93,13 @@ def draw_cmap(cmap, values=np.array([[0, 1]]), **kwargs):
     Parameters:
         - cmap: A matplotlib colormap
         - values: The values to use for the colormap, defaults to [0, 1]
-        - **kwargs: Additional keyword arguments to pass to `plt.colorbar()`
-        
+        - kwargs: Additional keyword arguments to pass to `plt.colorbar()`
+
     Returns:
         A matplotlib colorbar, which you can save with:
         `plt.savefig(<file_name>, bbox_inches='tight')`
     """
     img = plt.imshow(values, cmap=cmap)
-    cbar = plt.colorbar(cmap=cmap, **kwargs)
+    cbar = plt.colorbar(**kwargs)
     img.axes.remove()
     return cbar
